5d98b73e632ea50ffab7bdd73c7122bffc8c6dcb
azizkeita <apple@apples-MacBook-Pro.local>
2025-12-27 00:19:06 +0000

    version last

---
+++ lien.py
@@ -0,0 +1,13 @@
+import random
+
+def generate_random_number():
+    return random.randint(1, 100)
+
+print(generate_random_number())
+
+def generate_random_number_between_1_and_100():
+    return random.randint(1, 100)
+
+print(generate_random_number_between_1_and_100())
+
+
